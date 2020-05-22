769b34c5517069ffbf1e740fb9d538f425584546
aradhana160 <64077295+aradhana160@users.noreply.github.com>
2020-05-22 14:58:04 +0530

    Aradhana : Interaction Behaviour and Payment Behaviour initial version

---
+++ src/com/account360i/postenrichment/InteractionBehaviourSummary.java
@@ -0,0 +1,137 @@
+package com.account360i.postenrichment;
+
+import java.util.ArrayList;
+import java.util.Collection;
+
+import org.apache.log4j.Logger;
+
+import com.allsight.Party;
+import com.allsight.Party.EmailFromParty;
+import com.allsight.Party.InteractionBehaviour;
+import com.allsight.Party.ProductMention;
+import com.allsight.Party.Webchat;
+import com.allsight.enrichment.common.EnrichmentFunction;
+import com.allsight.entity.impl.Entity;
+
+public class InteractionBehaviourSummary extends EnrichmentFunction {
+	private static final Logger logger = Logger.getLogger(InteractionBehaviourSummary.class);
+
+	@Override
+	public Object applyEnrichment(Entity<?> entity) throws Exception {
+		// TODO Auto-generated method stub
+		
+		if (!getExecutionContext().isEIDRecord()) {
+		      logger.warn("This enrichment expects an EID record. Skipping enrichment");
+		      return null;
+		} 
+		  
+		Party asparty = interactionSummary(entity);
+		logger.debug("asparty :: " + asparty);
+		  
+		return asparty;
+	}
+	
+
+	private Party interactionSummary(Entity<?> entity) {
+		// TODO Auto-generated method stub
+		Integer totalEmails = 0;
+		Integer totalWebchats = 0;
+		Integer totalInteractions = 0;
+		String preferredMedium;
+		
+		if (((Party) entity).getInteractions() != null && ((Party) entity).getInteractions().getEmailFromParty()!= null) {
+			Collection<EmailFromParty> emails = ((Party) entity).getInteractions().getEmailFromParty();
+			totalEmails = emails.size();
+		}
+		if (((Party) entity).getInteractions() != null && ((Party) entity).getInteractions().getWebchat()!= null) {
+			Collection<Webchat> webchats = ((Party) entity).getInteractions().getWebchat();
+			totalWebchats = webchats.size();
+		}
+		
+		if(totalEmails > totalWebchats) {
+			preferredMedium = "Email";
+		}
+		else if(totalEmails < totalWebchats) {
+			preferredMedium = "Webchat";
+		}
+		else {
+			preferredMedium = "Email/Webchat";
+		}
+		
+		totalInteractions = totalEmails + totalWebchats;
+		
+		String sentiment = productSentimentAggregate(entity);
+		
+		Party asparty = (Party)entity;
+		Party.Insights insight = new Party.Insights();
+		InteractionBehaviour interaction = new InteractionBehaviour();
+		interaction.setKey("1");
+		interaction.setPreferredMedium(preferredMedium);
+		interaction.setTotalEmailInteractions(totalEmails.toString());
+		interaction.setTotalWebchatInteractions(totalWebchats.toString());
+		interaction.setTotalInteractions(totalInteractions.toString());
+		interaction.setAvgInteractionProductSentiment(sentiment);
+		
+		Collection<InteractionBehaviour> coll = new ArrayList<InteractionBehaviour>();
+		coll.add(interaction);
+		
+		insight.setInteractionBehaviour(coll);
+		asparty.setInsights(insight);
+		
+		logger.debug("totalInteractions : " + totalInteractions);
+		logger.debug("totalEmails : " + totalEmails);
+		logger.debug("totalWebchats : " + totalWebchats);
+		logger.debug("preferredMedium : " + preferredMedium);
+		
+		
+		return asparty;
+	}
+	
+	public String productSentimentAggregate(Entity entity) {
+		
+		String finalSentiment = "";
+		int positiveCount = 0;
+		int negativeCount = 0;
+		int neutralCount = 0;
+		
+		if (((Party) entity).getSocialInteractions() != null && ((Party) entity).getSocialInteractions().getMentions()!= null && 
+				((Party) entity).getSocialInteractions().getMentions().getProductMention()!= null) {
+			Collection<ProductMention> mentions = ((Party) entity).getSocialInteractions().getMentions().getProductMention();
+			
+			for (ProductMention mention : mentions) {
+			
+				String sentiment = mention.getSentiment();
+				if(sentiment.equalsIgnoreCase("positive")) {
+					positiveCount++;
+				}
+				else if(sentiment.equalsIgnoreCase("negative")) {
+					negativeCount++;
+				}
+				else {
+					neutralCount++;
+				}
+			}
+		}
+		
+		if(positiveCount >= negativeCount && positiveCount >= neutralCount) {
+			finalSentiment = "Positive";
+		}
+		else if(negativeCount >= neutralCount && negativeCount> positiveCount) {
+				finalSentiment = "Negative";
+		}
+		else if(neutralCount >= negativeCount && neutralCount > positiveCount) {
+			finalSentiment = "Neutral";
+		}
+		
+		logger.debug("sentiment : " + finalSentiment);
+		return finalSentiment;
+	}
+
+	@Override
+	public String standardize(String arg0) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+	
+	
+}
